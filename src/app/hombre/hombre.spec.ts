d66656dcea63dae8733d3b63e256469715d7ee6e
Flor-AC <florac206@gmail.com>
2020-11-24 09:21:17 -0600

    se corrigieron algunos errores

--- src/app/hombre/hombre.spec.ts
+++ src/app/hombre/hombre.spec.ts
@@ -1,90 +1,42 @@
-import { Hombre, pesoMax, pesoMin } from "./Hombre"
+import { Hombre, pesoMax, pesoMin } from './hombre';
 
 
 describe('Hombre uni tests', () => {
-    
+
     it('Recibo estatura de 1.80, peso de 80 Devuelve IMC de 24.69, peso normal' , () => {
-        //Pattern AAA (Arrage - Act - Assert)
-
-        //Arrange
         let result = '';
-
-        //Act
-        result = Hombre(1.80,80);
-
-        //Assert
+        result = Hombre(1.80, 80);
         expect(result).toBe('IMC: 24.69, peso normal');
 
-    })
+    });
 
     it('Recibo la estatura de 1.80 y edad 28 tiene un Rango de peso 71.28 kg - 87.48 kg' , () => {
-        //Pattern AAA (Arrage - Act - Assert)
-
-        //Arrange
         let result = '';
-
-        //Act
-        result = 'Rango de peso '+pesoMin(28,1.80)+' - '+pesoMax(28,1.80);
-
-        //Assert
+        result = 'Rango de peso ' + pesoMin(28, 1.80) + ' - ' + pesoMax(28, 1.80);
         expect(result).toBe('Rango de peso 71.28 kg - 87.48 kg');
-
-    })
+    });
 
     it('Recibo estatura de 1.50, peso de 45 Devuelve IMC de 20.00, peso normal' , () => {
-        //Pattern AAA (Arrage - Act - Assert)
-
-        //Arrange
         let result = '';
-
-        //Act
-        result = Hombre(1.50,45);
-
-        //Assert
+        result = Hombre(1.50, 45);
         expect(result).toBe('IMC: 20.00, peso normal');
-
-    })
+    });
 
     it('Recibo la estatura de 1.50 y edad 48 tiene un Rango de peso 51.75 kg - 85.50 kg' , () => {
-        //Pattern AAA (Arrage - Act - Assert)
-
-        //Arrange
         let result = '';
-
-        //Act
-        result = 'Rango de peso '+pesoMin(48,1.50)+' - '+pesoMax(48,1.50);
-
-        //Assert
+        result = 'Rango de peso ' + pesoMin(48, 1.50) + ' - ' + pesoMax(48, 1.50);
         expect(result).toBe('Rango de peso 51.75 kg - 85.50 kg');
-
-    })
+    });
 
     it('Recibo estatura de 1.90, peso de 80 Devuelve IMC de 22.16, peso normal' , () => {
-        //Pattern AAA (Arrage - Act - Assert)
-
-        //Arrange
         let result = '';
-
-        //Act
-        result = Hombre(1.90,80);
-
-        //Assert
+        result = Hombre(1.90, 80);
         expect(result).toBe('IMC: 22.16, peso normal');
+    });
 
-    })
-   
     it('Recibo la estatura de 1.90 y edad 25 tiene un Rango de peso 79.42 kg - 97.47 kg' , () => {
-        //Pattern AAA (Arrage - Act - Assert)
-
-        //Arrange
         let result = '';
-
-        //Act
-        result = 'Rango de peso '+pesoMin(25,1.90)+' - '+pesoMax(25,1.90);
-
-        //Assert
+        result = 'Rango de peso ' + pesoMin(25, 1.90) + ' - ' + pesoMax(25, 1.90);
         expect(result).toBe('Rango de peso 79.42 kg - 97.47 kg');
-
-    })
-
-})
+    });
+});
--- src/app/hombre/hombre.ts
+++ src/app/hombre/hombre.ts
@@ -1,42 +1,44 @@
-export function Hombre(estatura: number,peso: number) {
-    let IMC = peso/(estatura*estatura);
+export function Hombre(estatura: number, peso: number) {
+    const IMC = peso / (estatura * estatura);
     IMC.toFixed(2);
     let rest;
-    if (IMC<20) {
-        rest = `peso bajo` 
-    } else {if (IMC >=20 && IMC <=27) {
-            rest = `peso normal` 
-        } else {if (IMC >=27 && IMC <=30) {
-            rest = `sobrepeso` 
-            } else {if (IMC >=30 && IMC <=35) {
-                    rest = `obesidad grado 1` 
-                } else {if (IMC >=35 && IMC <=40) {
-                        rest = `obesidad grado 2` 
-                    } else {rest = `obesidad grado 3 (obesidad mórbida)` 
+    if (IMC < 20) {
+        rest = `peso bajo`;
+    } else {if (IMC >= 20 && IMC <= 27) {
+            rest = `peso normal`;
+        } else {if (IMC >= 27 && IMC <= 30) {
+            rest = `sobrepeso`;
+            } else {if (IMC >= 30 && IMC <= 35) {
+                    rest = `obesidad grado 1`;
+                } else {if (IMC >= 35 && IMC <= 40) {
+                        rest = `obesidad grado 2`;
+                    } else {rest = `obesidad grado 3 (obesidad mórbida)`;
                     }
                 }
             }
         }
     }
-    return "IMC: "+IMC.toFixed(2)+", "+rest
+    return 'IMC: ' + IMC.toFixed(2) + ', ' + rest;
     }
 
-export function pesoMin(edad: number,estatura: number){
-    let rest: number
-    if (edad==16) {
-        rest = (estatura*estatura)*19;
-    } else {if (edad==17||edad==18) {
-            rest = (estatura*estatura)*20
-        } else {if (edad>=19 && edad<=24) {
-                rest = (estatura*estatura)*21
-            } else {if (edad>=25 && edad<=34) {
-                    rest = (estatura*estatura)*22
-                } else {if (edad>=35 && edad<=54) {
-                        rest = (estatura*estatura)*23
-                    } else {if (edad>=55 && edad<=64) {
-                            rest = (estatura*estatura)*24
-                        } else {if (edad>=65 && edad<=90) {
-                                rest = (estatura*estatura)*25
+export function pesoMin(edad: number, estatura: number){
+    let rest: number;
+  // tslint:disable-next-line:triple-equals
+    if (edad == 16) {
+        rest = (estatura * estatura) * 19;
+      // tslint:disable-next-line:triple-equals
+    } else {if (edad == 17 || edad == 18) {
+            rest = (estatura * estatura) * 20;
+        } else {if (edad >= 19 && edad <= 24) {
+                rest = (estatura * estatura) * 21;
+            } else {if (edad >= 25 && edad <= 34) {
+                    rest = (estatura * estatura) * 22;
+                } else {if (edad >= 35 && edad <= 54) {
+                        rest = (estatura * estatura) * 23;
+                    } else {if (edad >= 55 && edad <= 64) {
+                            rest = (estatura * estatura) * 24;
+                        } else {if (edad >= 65 && edad <= 90) {
+                                rest = (estatura * estatura) * 25;
                             }
                         }
                     }
@@ -45,30 +47,32 @@ export function pesoMin(edad: number,estatura: number){
         }
     }
 
-    return rest.toFixed(2)+" kg";
+    return rest.toFixed(2) + ' kg';
 }
 
-export function pesoMax(edad: number,estatura: number){
-    let rest: number
-    if (edad==16) {
-        rest = (estatura*estatura)*24
+export function pesoMax(edad: number, estatura: number){
+    let rest: number;
+  // tslint:disable-next-line:triple-equals
+    if (edad == 16) {
+        rest = (estatura * estatura) * 24;
     } else {
-        if (edad==17||edad==18) {
-            rest = (estatura*estatura)*25
+      // tslint:disable-next-line:triple-equals
+        if (edad == 17 || edad == 18) {
+            rest = (estatura * estatura) * 25;
         } else {
-            if (edad>=19 && edad<=24) {
-                rest = (estatura*estatura)*26
+            if (edad >= 19 && edad <= 24) {
+                rest = (estatura * estatura) * 26;
             } else {
-                if (edad>=25 && edad<=34) {
-                    rest = (estatura*estatura)*27
+                if (edad >= 25 && edad <= 34) {
+                    rest = (estatura * estatura) * 27;
                 } else {
-                    if (edad>=35 && edad<=54) {
-                        rest = (estatura*estatura)*38
+                    if (edad >= 35 && edad <= 54) {
+                        rest = (estatura * estatura) * 38;
                     } else {
-                        if (edad>=55 && edad<=64) {
-                            rest = (estatura*estatura)*29
-                        } else {if (edad>=65 && edad<=90) {
-                                rest = (estatura*estatura)*30
+                        if (edad >= 55 && edad <= 64) {
+                            rest = (estatura * estatura) * 29;
+                        } else {if (edad >= 65 && edad <= 90) {
+                                rest = (estatura * estatura) * 30;
                             }
                         }
                     }
@@ -76,6 +80,6 @@ export function pesoMax(edad: number,estatura: number){
             }
         }
     }
-    return rest.toFixed(2)+" kg";
+    return rest.toFixed(2) + ' kg';
 
 }
