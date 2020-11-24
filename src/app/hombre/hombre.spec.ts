8a8369d7d71c61fcaa2a09398c73bd2d03849db1
Dantesnake <41603578+Dantesnake@users.noreply.github.com>
2020-11-23 22:19:32 -0600

    Se Corrigieron errores pequeños #2

--- src/app/hombre/hombre.spec.ts
+++ src/app/hombre/hombre.spec.ts
@@ -3,7 +3,7 @@ import { Hombre, pesoMax, pesoMin } from "./Hombre"
 
 describe('Hombre uni tests', () => {
     
-    it('Should IMC estatura: 1.80, peso: 80 = IMC: 24.69, peso normal' , () => {
+    it('Recibo estatura de 1.80, peso de 80 Devuelve IMC de 24.69, peso normal' , () => {
         //Pattern AAA (Arrage - Act - Assert)
 
         //Arrange
@@ -17,7 +17,7 @@ describe('Hombre uni tests', () => {
 
     })
 
-    it('Should peso ideal de la estatura: 1.80 y edad 28 = Rango de peso 71.28 kg - 87.48 kg' , () => {
+    it('Recibo la estatura de 1.80 y edad 28 tiene un Rango de peso 71.28 kg - 87.48 kg' , () => {
         //Pattern AAA (Arrage - Act - Assert)
 
         //Arrange
@@ -31,7 +31,7 @@ describe('Hombre uni tests', () => {
 
     })
 
-    it('Should IMC estatura: 1.50, peso: 45 = IMC: 20.00, peso normal' , () => {
+    it('Recibo estatura de 1.50, peso de 45 Devuelve IMC de 20.00, peso normal' , () => {
         //Pattern AAA (Arrage - Act - Assert)
 
         //Arrange
@@ -45,7 +45,7 @@ describe('Hombre uni tests', () => {
 
     })
 
-    it('Should peso ideal de la estatura: 1.50 y edad 48 = Rango de peso 51.75 kg - 85.50 kg' , () => {
+    it('Recibo la estatura de 1.50 y edad 48 tiene un Rango de peso 51.75 kg - 85.50 kg' , () => {
         //Pattern AAA (Arrage - Act - Assert)
 
         //Arrange
@@ -59,7 +59,7 @@ describe('Hombre uni tests', () => {
 
     })
 
-    it('Should IMC estatura: 1.90, peso: 80 = IMC: 22.16, peso normal' , () => {
+    it('Recibo estatura de 1.90, peso de 80 Devuelve IMC de 22.16, peso normal' , () => {
         //Pattern AAA (Arrage - Act - Assert)
 
         //Arrange
@@ -73,7 +73,7 @@ describe('Hombre uni tests', () => {
 
     })
    
-    it('Should peso ideal de la estatura: 1.90 y edad 25 = Rango de peso 79.42 kg - 97.47 kg' , () => {
+    it('Recibo la estatura de 1.90 y edad 25 tiene un Rango de peso 79.42 kg - 97.47 kg' , () => {
         //Pattern AAA (Arrage - Act - Assert)
 
         //Arrange
